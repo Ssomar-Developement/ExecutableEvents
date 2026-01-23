07a2b8ac1e35860978a3693b6edb4f7d043b1648
Josef Miko Urquico <josef.urquico@gmail.com>
2026-01-23 14:13:08 +0800

    Fixing the return value of %advancements%
    As of this writing, EE suddenly has issues for 1.13 to 1.15. Meaning I can't check if this commit's changes are working fine in those versions so I will push this commit for now

--- src/main/java/com/ssomar/executableevents/events/player/custom/PlayerAdvancementDoneListener.java
+++ src/main/java/com/ssomar/executableevents/events/player/custom/PlayerAdvancementDoneListener.java
@@ -2,12 +2,16 @@ package com.ssomar.executableevents.events.player.custom;
 
 import com.ssomar.executableevents.events.EventsManager;
 import com.ssomar.executableevents.executableevents.activators.Option;
+import com.ssomar.score.SCore;
 import com.ssomar.score.sobject.sactivator.EventInfo;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class PlayerAdvancementDoneListener implements Listener {
@@ -16,11 +20,15 @@ public class PlayerAdvancementDoneListener implements Listener {
     public void onPlayerAdvancementDoneEvent(PlayerAdvancementDoneEvent e) {
         EventInfo eInfo = new EventInfo(e);
         eInfo.setPlayer(Optional.of(e.getPlayer()));
-        try {
-            String advancement = PlainTextComponentSerializer.plainText().serialize(e.getAdvancement().getDisplay().displayName());
-            advancement = advancement.substring(1, advancement.length() - 1);
-            eInfo.getPlaceholders().put("%advancement%", advancement);
-        } catch (Exception exception) {}
+        // 1.12 api doesn't return advancement's name
+        if (SCore.is1v13Less())
+            try {
+                String advancement = "null";
+                advancement = PlainTextComponentSerializer.plainText().serialize(e.getAdvancement().getDisplay().displayName());
+                // remove the brackets
+                advancement = advancement.substring(1, advancement.length() - 1);
+                eInfo.getPlaceholders().put("%advancement%", advancement);
+            } catch (Exception exception) {}
         eInfo.setOption(Option.PLAYER_ADVANCEMENT);
         EventsManager.getInstance().activeOption(eInfo);
     }
