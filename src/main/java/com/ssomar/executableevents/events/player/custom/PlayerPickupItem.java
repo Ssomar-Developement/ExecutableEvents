8d239f931090e9663c3dcf8733fdf3cf599a5eb3
Josef Miko Urquico <josef.urquico@gmail.com>
2026-02-06 11:39:52 +0800

    Double checked issues across supported versions and adjusted the logic accordingly

--- src/main/java/com/ssomar/executableevents/events/player/custom/PlayerPickupItem.java
+++ src/main/java/com/ssomar/executableevents/events/player/custom/PlayerPickupItem.java
@@ -27,15 +27,18 @@ public class PlayerPickupItem implements Listener {
         eInfo.setPlayer(Optional.of(((Player) e.getEntity()).getPlayer()));
         eInfo.setItem(Optional.of(e.getItem().getItemStack()));
         eInfo.setTargetEntity(Optional.of(e.getItem()));
-        eInfo.getPlaceholders().put("%item_type%", e.getItem().getItemStack().getType().toString());
-        eInfo.getPlaceholders().put("%item_name%", e.getItem().getItemStack().getItemMeta().getItemName().toString());
-        eInfo.getPlaceholders().put("%item_amount%", String.valueOf(e.getItem().getItemStack().getAmount()));
+
+        if (SCore.is1v13() || SCore.is1v14() || SCore.is1v15() || SCore.is1v16Plus()) {
+            eInfo.getPlaceholders().put("%item_type%", e.getItem().getItemStack().getType().toString());
+            eInfo.getPlaceholders().put("%item_name%", e.getItem().getItemStack().getItemMeta().getDisplayName().toString());
+            eInfo.getPlaceholders().put("%item_amount%", String.valueOf(e.getItem().getItemStack().getAmount()));
+        }
 
         if (SCore.is1v21v4Plus()) {
             CustomModelDataComponent cmdataComp = e.getItem().getItemStack().getItemMeta().getCustomModelDataComponent();
-            eInfo.getPlaceholders().put("%item_cmdata_f_0%", cmdataComp.getFloats().size() > 0 ? cmdataComp.getFloats().getFirst().toString() : "-1");
-            eInfo.getPlaceholders().put("%item_cmdata_s_0%", cmdataComp.getStrings().size() > 0 ? cmdataComp.getStrings().getFirst() : "null");
-        } else {
+            eInfo.getPlaceholders().put("%item_cmdata_f_0%", !cmdataComp.getFloats().isEmpty() ? cmdataComp.getFloats().getFirst().toString() : "-1");
+            eInfo.getPlaceholders().put("%item_cmdata_s_0%", !cmdataComp.getStrings().isEmpty() ? cmdataComp.getStrings().getFirst() : "null");
+        } else if (SCore.is1v14() || SCore.is1v15() || SCore.is1v16Plus()) { // Custom Model Data was implemented in 1.14
             ItemMeta itemmeta = e.getItem().getItemStack().getItemMeta();
             if (itemmeta != null && itemmeta.hasCustomModelData())
                 eInfo.getPlaceholders().put("%item_cmdata%", String.valueOf(e.getItem().getItemStack().getItemMeta().getCustomModelData()));
